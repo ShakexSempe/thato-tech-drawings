faa770c2f5c1bc9ad47b95d50f28b66ab7f36b4a
Ray-Shakex <bshakex@gmail.com>
2021-04-26 12:25:14 +0200

    remove sidebar after selecting lnk

--- js/sidebar-toggle.js
+++ js/sidebar-toggle.js
@@ -1,6 +1,47 @@
 const menuToggleButton = document.querySelector('.menu-toggle');
 const sidebar = document.querySelector('.sidebar');
+const sideLinks = document.querySelectorAll('.sidebar .sidebar-nav-list li a')
 
 menuToggleButton.addEventListener('click', () => {
-    sidebar.classList.toggle('open')
-})
+    sidebar.classList.toggle('open');
+});
+
+sideLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        sidebar.classList.toggle('open');
+    })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(sideLinks);
+// menuToggleButton.addEventListener('click', () => {
+//     console.log(sideLinks)
+//     sidebar.classList.toggle('open')
+// });
+
+// sideLinks.forEach(link => {
+//     link.addEventListener('click', () => {
+//         sidebar.classList.toggle('open')
+//     })
+// })
