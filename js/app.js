57cb0271cf5a1c8f22bef91f8f8f5de2a1d3fd46
Ray-Shakex <bshakex@gmail.com>
2022-04-26 10:26:21 +0200

    sass: logo and banner

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const logo = document.querySelector('.logo');
+console.log(logo);
+const banner = document.querySelector('.banner');
+
+const logoOptions = {
+    rootMargin: "-95% 0px 0px 0px",
+}
+
+const bannerObserver = new IntersectionObserver(
+    function(entries, bannerObserver){
+        entries.forEach(entry => {
+            if(!entry.isIntersecting){
+                console.log("banner NOT IO");
+                logo.classList.remove("active-logo");
+            } else {
+                console.log("banner IS IO");
+                logo.classList.add("active-logo");
+            };
+        });
+    }, logoOptions
+);
+bannerObserver.observe(banner);
